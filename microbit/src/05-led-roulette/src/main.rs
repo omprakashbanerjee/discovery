4eb3cb81cbb9511fafe908ddd911183ee939a6df
Om Prakash Banerjee <omprakashbanerjeee@gmail.com>
2022-06-17 15:31:40 +0530

    code modified

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -1,17 +1,137 @@
+// #![deny(unsafe_code)]
+// #![no_main]
+// #![no_std]
+
+// use cortex_m_rt::entry;
+// use panic_halt as _;
+// use microbit as _;
+
+// #[entry]
+// fn main() -> ! {
+//     let _y;
+//     let x = 42;
+//     _y = x;
+//     // infinite loop; just so we don't leave this stack frame
+//     loop {}
+// }
+
+// #![deny(unsafe_code)]
+// #![no_main]
+// #![no_std]
+
+// use cortex_m_rt::entry;
+// use panic_halt as _;
+// use microbit::board::Board;
+// use microbit::hal::prelude::*;
+
+// #[entry]
+// fn main() -> ! {
+//     let mut board = Board::take().unwrap();
+
+//     board.display_pins.col1.set_low().unwrap();
+//     board.display_pins.row1.set_high().unwrap();
+
+//     loop {}
+// }
+
+
+// #![deny(unsafe_code)]
+// #![no_main]
+// #![no_std]
+
+// use cortex_m_rt::entry;
+// use rtt_target::{rtt_init_print, rprintln};
+// use panic_rtt_target as _;
+// use microbit::board::Board;
+// use microbit::hal::timer::Timer;
+// use microbit::hal::prelude::*;
+
+// #[entry]
+// fn main() -> ! {
+//     rtt_init_print!();
+//     let mut board = Board::take().unwrap();
+
+//     let mut timer = Timer::new(board.TIMER0);
+
+//     loop {
+//         timer.delay_ms(1000u16);
+//         rprintln!("1000 ms passed");
+//     }
+// }
+
+
+// #![deny(unsafe_code)]
+// #![no_main]
+// #![no_std]
+
+// use cortex_m_rt::entry;
+// use rtt_target::{rtt_init_print, rprintln};
+// use panic_rtt_target as _;
+// use microbit::board::Board;
+// use microbit::hal::timer::Timer;
+// use microbit::hal::prelude::*;
+
+// #[entry]
+// fn main() -> ! {
+//     rtt_init_print!();
+//     let mut board = Board::take().unwrap();
+
+//     let mut timer = Timer::new(board.TIMER0);
+
+//     board.display_pins.col1.set_low().unwrap();
+//     let mut row1 = board.display_pins.row1;
+
+//     loop {
+//         row1.set_low().unwrap();
+//         rprintln!("Dark!");
+//         timer.delay_ms(1_000_u16);
+//         row1.set_high().unwrap();
+//         rprintln!("Light!");
+//         timer.delay_ms(1_000_u16);
+//     }
+// }
+
 #![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
-use microbit as _;
+use rtt_target::rtt_init_print;
+use panic_rtt_target as _;
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::Timer,
+};
+
+const PIXELS: [(usize, usize); 16] = [
+    (0,0), (0,1), (0,2), (0,3), (0,4), (1,4), (2,4), (3,4), (4,4),
+    (4,3), (4,2), (4,1), (4,0), (3,0), (2,0), (1,0)
+];
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    rtt_init_print!();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+    let mut leds = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+
+    let mut last_led = (0,0);
+
+    loop {
+        for current_led in PIXELS.iter() {
+            leds[last_led.0][last_led.1] = 0;
+            leds[current_led.0][current_led.1] = 1;
+            display.show(&mut timer, leds, 30);
+            last_led = *current_led;
+        }
+    }
 }
--- microbit/src/08-i2c/src/main.rs
+++ microbit/src/08-i2c/src/main.rs
@@ -1,52 +1,212 @@
-#![deny(unsafe_code)]
+// #![deny(unsafe_code)]
+// #![no_main]
+// #![no_std]
+
+// use cortex_m_rt::entry;
+// use rtt_target::{rtt_init_print, rprintln};
+// use panic_rtt_target as _;
+
+// use microbit::hal::prelude::*;
+
+// #[cfg(feature = "v1")]
+// use microbit::{
+//     hal::twi,
+//     pac::twi0::frequency::FREQUENCY_A,
+// };
+
+// #[cfg(feature = "v2")]
+// use microbit::{
+//     hal::twim,
+//     pac::twim0::frequency::FREQUENCY_A,
+// };
+
+// const ACCELEROMETER_ADDR: u8 = 0b0011001;
+// const MAGNETOMETER_ADDR: u8 = 0b0011110;
+
+// const ACCELEROMETER_ID_REG: u8 = 0x0f;
+// const MAGNETOMETER_ID_REG: u8 = 0x4f;
+
+// #[entry]
+// fn main() -> ! {
+//     rtt_init_print!();
+//     let board = microbit::Board::take().unwrap();
+
+
+//     #[cfg(feature = "v1")]
+//     let mut i2c = { twi::Twi::new(board.TWI0, board.i2c.into(), FREQUENCY_A::K100) };
+
+//     #[cfg(feature = "v2")]
+//     let mut i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
+
+//     let mut acc = [0];
+//     let mut mag = [0];
+
+//     // First write the address + register onto the bus, then read the chip's responses
+//     i2c.write_read(ACCELEROMETER_ADDR, &[ACCELEROMETER_ID_REG], &mut acc).unwrap();
+//     i2c.write_read(MAGNETOMETER_ADDR, &[MAGNETOMETER_ID_REG], &mut mag).unwrap();
+
+//     rprintln!("The accelerometer chip's id is: {:#b}", acc[0]);
+//     rprintln!("The magnetometer chip's id is: {:#b}", mag[0]);
+
+//     loop {}
+// }
+
+
+// #![deny(unsafe_code)]
+// #![no_main]
+// #![no_std]
+
+// use cortex_m_rt::entry;
+// use rtt_target::{rtt_init_print, rprintln};
+// use panic_rtt_target as _;
+
+// #[cfg(feature = "v1")]
+// use microbit::{
+//     hal::twi,
+//     pac::twi0::frequency::FREQUENCY_A,
+// };
+
+// #[cfg(feature = "v2")]
+// use microbit::{
+//     hal::twim,
+//     pac::twim0::frequency::FREQUENCY_A,
+// };
+
+// use lsm303agr::{
+//     AccelOutputDataRate, Lsm303agr,
+// };
+
+// #[entry]
+// fn main() -> ! {
+//     rtt_init_print!();
+//     let board = microbit::Board::take().unwrap();
+
+
+//     #[cfg(feature = "v1")]
+//     let i2c = { twi::Twi::new(board.TWI0, board.i2c.into(), FREQUENCY_A::K100) };
+
+//     #[cfg(feature = "v2")]
+//     let i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
+
+//     // Code from documentation
+//     let mut sensor = Lsm303agr::new_with_i2c(i2c);
+//     sensor.init().unwrap();
+//     sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
+//     loop {
+//         if sensor.accel_status().unwrap().xyz_new_data {
+//             let data = sensor.accel_data().unwrap();
+//             // RTT instead of normal print
+//             rprintln!("Acceleration: x {} y {} z {}", data.x, data.y, data.z);
+//         }
+//     }
+// }
+
 #![no_main]
 #![no_std]
 
-use cortex_m_rt::entry;
-use rtt_target::{rtt_init_print, rprintln};
-use panic_rtt_target as _;
+use core::str;
 
-use microbit::hal::prelude::*;
+use cortex_m_rt::entry;
+use rtt_target::rtt_init_print;
+use panic_rtt_target as _;
 
 #[cfg(feature = "v1")]
 use microbit::{
     hal::twi,
     pac::twi0::frequency::FREQUENCY_A,
+    hal::uart,
+    hal::uart::{Baudrate, Parity},
 };
 
 #[cfg(feature = "v2")]
 use microbit::{
     hal::twim,
     pac::twim0::frequency::FREQUENCY_A,
+    hal::uarte,
+    hal::uarte::{Baudrate, Parity},
 };
 
-const ACCELEROMETER_ADDR: u8 = 0b0011001;
-const MAGNETOMETER_ADDR: u8 = 0b0011110;
+use microbit::hal::prelude::*;
+use lsm303agr::{AccelOutputDataRate, MagOutputDataRate, Lsm303agr};
+use heapless::Vec;
+use nb::block;
+use core::fmt::Write;
 
-const ACCELEROMETER_ID_REG: u8 = 0x0f;
-const MAGNETOMETER_ID_REG: u8 = 0x4f;
+#[cfg(feature = "v2")]
+mod serial_setup;
+#[cfg(feature = "v2")]
+use serial_setup::UartePort;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
 
-
     #[cfg(feature = "v1")]
-    let mut i2c = { twi::Twi::new(board.TWI0, board.i2c.into(), FREQUENCY_A::K100) };
+    let mut serial = {
+        uart::Uart::new(
+            board.UART0,
+            board.uart.into(),
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        )
+    };
 
     #[cfg(feature = "v2")]
-    let mut i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
+    let mut serial = {
+        let serial = uarte::Uarte::new(
+            board.UARTE0,
+            board.uart.into(),
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        );
+        UartePort::new(serial)
+    };
 
-    let mut acc = [0];
-    let mut mag = [0];
+    #[cfg(feature = "v1")]
+    let i2c = { twi::Twi::new(board.TWI0, board.i2c.into(), FREQUENCY_A::K100) };
 
-    // First write the address + register onto the bus, then read the chip's responses
-    i2c.write_read(ACCELEROMETER_ADDR, &[ACCELEROMETER_ID_REG], &mut acc).unwrap();
-    i2c.write_read(MAGNETOMETER_ADDR, &[MAGNETOMETER_ID_REG], &mut mag).unwrap();
+    #[cfg(feature = "v2")]
+    let i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
 
-    rprintln!("The accelerometer chip's id is: {:#b}", acc[0]);
-    rprintln!("The magnetometer chip's id is: {:#b}", mag[0]);
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
+    sensor.set_mag_odr(MagOutputDataRate::Hz50).unwrap();
+    let mut sensor = sensor.into_mag_continuous().ok().unwrap();
 
-    loop {}
+    loop {
+        let mut buffer: Vec<u8, 32> = Vec::new();
+
+        loop {
+            let byte = block!(serial.read()).unwrap();
+
+            if byte == 13 {
+                break;
+            }
+
+            if buffer.push(byte).is_err() {
+                write!(serial, "error: buffer full\r\n").unwrap();
+                break;
+            }
+        }
+
+        if str::from_utf8(&buffer).unwrap().trim() == "accelerometer" {
+            while !sensor.accel_status().unwrap().xyz_new_data  {
+            }
+
+            let data = sensor.accel_data().unwrap();
+            write!(serial, "Accelerometer: x {} y {} z {}\r\n", data.x, data.y, data.z).unwrap();
+        } else if str::from_utf8(&buffer).unwrap().trim() == "magnetometer" {
+            while !sensor.mag_status().unwrap().xyz_new_data  {
+            }
+
+            let data = sensor.mag_data().unwrap();
+            write!(serial, "Magnetometer: x {} y {} z {}\r\n", data.x, data.y, data.z).unwrap();
+        } else {
+            write!(serial, "error: command not detected\r\n").unwrap();
+        }
+    }
 }
+
+
